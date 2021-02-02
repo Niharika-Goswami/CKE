95269a85ce5ebf497768bc2807cdf1d16d2451df
Niharika Goswami <76028608+Niharika-Goswami@users.noreply.github.com>
2021-02-02 09:53:26 +0530

    Add files via upload

---
+++ pp006.c
@@ -0,0 +1,19 @@
+//pp006:
+//Write a program to create an array of 10 integers, input the data and show all even numbers in given numbers.
+#include<stdio.h>
+int main()
+{
+	int i;
+	int n[9];
+	printf("Enter the data: \n");
+	for(i=0;i<=9;i++)
+	{
+		printf("Enter %d number: ",i+1);
+		scanf("%d",&n[i]);
+	}
+	for(i=0;i<=9;i++)
+	{
+		printf(" %d ",n[i]);
+	}
+	return 0;
+}
