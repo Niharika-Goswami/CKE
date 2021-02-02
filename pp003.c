ca64a44032dc73493c44e800fdda6cf818f6fd3c
Niharika Goswami <76028608+Niharika-Goswami@users.noreply.github.com>
2021-02-02 09:49:10 +0530

    Add files via upload

---
+++ pp003.c
@@ -0,0 +1,18 @@
+//pp003:
+//Write a program to input a number and check it to be even and odd using bitwise operator.
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number: ");
+	scanf("%d",&a);
+	if(a&1==0)
+	{
+		printf("Odd");
+	}
+	else
+	{
+		printf("Even");
+	}
+	return 0;
+}
