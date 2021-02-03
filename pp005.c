a952e3e0b01e4a20392421745ab05b66356a6e7f
Niharika Goswami <76028608+Niharika-Goswami@users.noreply.github.com>
2021-02-03 11:22:22 +0530

    Update pp005.c

--- pp005.c
+++ pp005.c
@@ -4,8 +4,11 @@
 int main()
 {
 	int l,i,h;
-	printf("Enter the range: ");
-	scanf("%d%d",&l,&h);
+	printf("Enter the first number of the range: ");
+	scanf("%d",&l);
+	printf("Enter the second number of range: ");
+	scanf("%d",&h);
+	printf("The even numbers in the given range are: ");
 	for(i=l;i<=h;i++)
 	{
 		if(i%2==0)
